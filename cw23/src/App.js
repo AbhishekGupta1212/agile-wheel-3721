67c1886175569a0d80f28e06bcf161b9ae31ad50
AbhishekGupta1212 <107456489+AbhishekGupta1212@users.noreply.github.com>
2023-02-22 18:22:05 +0530

    added products

--- cw23/src/App.js
+++ cw23/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Homepage from './Components/Homepage';
 import Footer from './Components/Footer';
+import Mens from './Components/Mens';
+import Womens from './Components/Womens';
 
 function App() {
   return (
     <div className="App">
-<Homepage/>
+{/* <Homepage/> */}
 {/* <Footer/> */}
+{/* <Mens/> */}
+<Womens/>
     </div>
   );
 }
---
+++ cw23/src/Components/Mens.jsx
@@ -0,0 +1,76 @@
+import { Card, CardBody,Image,Text ,Img} from "@chakra-ui/react"
+import { useReducer , useEffect } from "react";
+import axios from 'axios'
+const initialState = {
+    data: [],
+    isLoading: false,
+    error: null,
+  };
+  const reducer = (state,action) => {
+    switch(action.type){
+      case "Request":
+        return{
+        ...state,
+        isLoading:true,
+      error:false
+      }
+      case "Success" :
+        return{
+          ...state,
+          data:action.payload,
+          isLoading:false,
+        error:false
+        }
+        case "Failed" :
+          return{
+            ...state,
+            data:[],
+            isLoading:false,
+          error:action.payload
+          }
+        default :
+        throw new Error()
+    }
+    };
+     
+
+
+function Mens(){
+
+    const [state,dispatch]=useReducer(reducer,initialState)
+    const{data}=state
+
+    const getData=()=>{
+        dispatch({type:"Request"})
+
+        axios.get("http://localhost:4040/mens-products").then((res)=>{
+          dispatch({type:"Success" ,payload:res.data})
+      
+        console.log(res)
+      }).catch((err)=>
+        dispatch({type:"Failed",payload:err.message})
+        // console.log(err)
+      )
+    }
+    useEffect(()=>{
+        getData();
+        },[])
+        console.log(data)
+
+    return (
+        <div>
+            <Card>
+                {data.map((el)=>(
+ <CardBody key={el.id}>
+    <Img src={el.imageUrl}/>
+    <Text>{el.name}</Text>
+    <Text>{el.price.current.text}</Text>
+                </CardBody>
+                ))}
+               
+            </Card>
+        </div>
+    )
+
+}
+export default Mens
